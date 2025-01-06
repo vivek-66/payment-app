ee5c97375d138aabcf91da4e3e9ad8da3c8fb133
Thomas V Varghese <118618681+www-domain@users.noreply.github.com>
2025-01-06 14:48:32 +0530

    Update environment.ts

--- micro-services/frontend/src/environments/environment.ts
+++ micro-services/frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://52.90.147.63:80/api',
-    authServiceUrl: 'http://52.90.147.63:8000/api/auth',
-    paymentServiceUrl: 'http://52.90.147.63:8000/api/payments'
+    authServiceUrl: 'http://52.90.147.63:80/api/auth',
+    paymentServiceUrl: 'http://52.90.147.63:80/api/payments'
   };
